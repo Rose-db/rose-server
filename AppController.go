172d4dd081253fa2c69a469074e9aa8cffef3784
Mario Škrlec <mario@prototyp.digital>
2020-10-04 11:15:56 +0200

    Extracted insert/read/delete method to standalone functions

--- AppController.go
+++ AppController.go
@@ -17,7 +17,6 @@ type AppResult struct {
 
 func (a *AppController) Run(m *Metadata) (IError, *AppResult) {
 	var vErr IError
-	var idx uint
 
 	vErr = m.Validate()
 
@@ -26,52 +25,66 @@ func (a *AppController) Run(m *Metadata) (IError, *AppResult) {
 	}
 
 	if m.Method == InsertMethodType {
-		idx = a.Database.Insert(m.Id, m.Data)
-
-		a.FsJobQueue.Add(&Job{
-			Id:    idx,
-			Value: m.Data,
-		})
-
-		if a.FsJobQueue.LimitReached() {
-			a.FsJobQueue.Consume()
-		}
-
-		return nil, &AppResult{
-			Id:     idx,
-			Method: m.Method,
-			Status: FoundResultStatus,
-		}
+		return a.insert(m)
 	} else if m.Method == DeleteMethodType {
-		return nil, &AppResult{
-			Id:     1,
-			Method: m.Method,
-			Status: FoundResultStatus,
-		}
+		return a.delete(m)
 	} else if m.Method == ReadMethodType {
-		var res *DbReadResult
-		var err *DbReadError
-		res, err = a.Database.Read(m.Id)
-
-		if err != nil {
-			return nil, &AppResult{
-				Id:     0,
-				Method: m.Method,
-				Status: NotFoundResultStatus,
-				Reason: err.Error(),
-				Result: "",
-			}
-		}
-
-		return nil, &AppResult{
-			Id:     res.Idx,
-			Method: m.Method,
-			Status: FoundResultStatus,
-			Result: res.Result,
-		}
+		return a.read(m)
 	}
 
 	panic("Internal rose error. Unreachable code reached. None of the methods have executed but one should have.")
+}
+
+func (a *AppController) insert(m *Metadata) (IError, *AppResult) {
+	var idx uint
+
+	idx = a.Database.Insert(m.Id, m.Data)
+
+	/*		a.FsJobQueue.Add(&Job{
+				Id:    idx,
+				Value: m.Data,
+			})
+
+			if a.FsJobQueue.LimitReached() {
+				a.FsJobQueue.Consume()
+			}*/
+
+	return nil, &AppResult{
+		Id:     idx,
+		Method: m.Method,
+		Status: FoundResultStatus,
+	}
+}
+
+func (a *AppController) read(m *Metadata) (IError, *AppResult) {
+	var res *DbReadResult
+	var err *DbReadError
+	res, err = a.Database.Read(m.Id)
+
+	if err != nil {
+		return nil, &AppResult{
+			Id:     0,
+			Method: m.Method,
+			Status: NotFoundResultStatus,
+			Reason: err.Error(),
+			Result: "",
+		}
+	}
+
+	return nil, &AppResult{
+		Id:     res.Idx,
+		Method: m.Method,
+		Status: FoundResultStatus,
+		Result: res.Result,
+	}
+}
+
+func (a *AppController) delete(m *Metadata) (IError, *AppResult) {
+	return nil, &AppResult{
+		Id:     1,
+		Method: m.Method,
+		Status: FoundResultStatus,
+	}
 }
 
 func (a *AppController) Init(log bool) chan IError {
